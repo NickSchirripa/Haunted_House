3ee7e63d651cba063c84f8e3c04407c04151ca95
NickSchirripa <108542730+NickSchirripa@users.noreply.github.com>
2025-09-17 15:35:35 -0400

    update
    -added floor
    -added Axis Helper
    -added bushes
    -added door
    -Made random graves using a For Loop, that generate on a circle

--- src/script.js
+++ src/script.js
@@ -1,107 +1,199 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { Timer } from 'three/addons/misc/Timer.js'
-import GUI from 'lil-gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Timer } from "three/addons/misc/Timer.js";
+import GUI from "lil-gui";
 
 /**
  * Base
  */
-// Debug
-const gui = new GUI()
+// Debug////////////////////////////////
+const gui = new GUI();
+
+//Debug Folders
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+//Axis Helper
+const axesHelper = new THREE.AxesHelper(5);
+
+scene.add(axesHelper);
 
 /**
- * House
+ * House/////////////////////////////////////////////
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
-)
-scene.add(sphere)
+//House Container
+const house = new THREE.Group();
+scene.add(house);
 
+//walls
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(4, 2.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+
+walls.position.y += 2.5 / 2;
+walls.material.wireframe = true;
+house.add(walls);
+
+//roof
+const roof = new THREE.Mesh(
+  new THREE.ConeGeometry(3.5, 1.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+
+roof.position.y = 2.5 + 0.75;
+roof.rotation.y = Math.PI * 0.25;
+house.add(roof);
+
+//door
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(2.2, 2.2),
+  new THREE.MeshStandardMaterial({
+    color: 0xff0000,
+  })
+);
+
+door.position.y = 1;
+door.position.z = 2 + 0.1;
+
+house.add(door);
+
+////////bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial();
+
+const bigFrontBush = new THREE.Mesh(bushGeometry, bushMaterial);
+bigFrontBush.scale.set(0.5, 0.5, 0.5);
+bigFrontBush.position.set(0.8, 0.2, 2.2);
+
+const smallFrontBush = new THREE.Mesh(bushGeometry, bushMaterial);
+smallFrontBush.scale.set(0.25, 0.25, 0.25);
+smallFrontBush.position.set(1.5, 0.1, 2.2);
+
+const leftBush = new THREE.Mesh(bushGeometry, bushMaterial);
+leftBush.scale.set(0.75, 0.75, 0.75);
+leftBush.position.set(-3, 0.5, -0.5);
+
+house.add(leftBush, bigFrontBush, smallFrontBush);
+
+///Graves/////
+const graves = new THREE.Group();
+scene.add(graves);
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial();
+
+for (let i = 0; i < 30; i++) {
+  //random circle angle
+  const angle = Math.random() * Math.PI * 2;
+  console.log(angle);
+
+  //set angle to sine and cosine for x and z cords
+  const x = Math.sin(angle);
+  const z = Math.cos(angle);
+
+  //mesh
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.x = x;
+  grave.position.z = z;
+
+  //add to graves group
+  graves.add(grave);
+}
+
+//Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial()
+);
+
+floor.rotation.x = -Math.PI * 0.5;
+
+scene.add(floor);
 /**
- * Lights
+ * Lights////////////////////////////////////////////////
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
-directionalLight.position.set(3, 2, -8)
-scene.add(directionalLight)
+const directionalLight = new THREE.DirectionalLight("#ffffff", 1.5);
+directionalLight.position.set(3, 2, -8);
+scene.add(directionalLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 4
-camera.position.y = 2
-camera.position.z = 5
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 5;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const timer = new Timer()
+const timer = new Timer();
 
-const tick = () =>
-{
-    // Timer
-    timer.update()
-    const elapsedTime = timer.getElapsed()
+const tick = () => {
+  // Timer
+  timer.update();
+  const elapsedTime = timer.getElapsed();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
